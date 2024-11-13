6127a0a7bfb30514976786bef9a2575ddd8f0bb9
Hunter <huntbarney0914@gmail.com>
2024-11-13 17:58:26 -0500

    Can connect to SFTP and list files on root.

--- go.mod
+++ go.mod
@@ -1,1 +1,10 @@
 module FTPArchive
+
+go 1.23.3
+
+require (
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/pkg/sftp v1.13.7 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
 	"log"
+	"strconv"
 )
 
 func main() {
@@ -17,5 +20,49 @@ func main() {
 		log.Fatal("Error loading profile:", err)
 	}
 
-	fmt.Println("Profile:", profile.HostName)
+	switch profile.Protocol {
+	case "FTP":
+		fmt.Println("FTP Connection")
+
+	case "SFTP":
+		ConnectSFTP(&profile)
+
+	default:
+		fmt.Println("Unknown protocol")
+	}
+}
+
+func ConnectSFTP(profile *Profile) {
+	fmt.Println("Connecting to SFTP site: ", profile.HostName)
+
+	sshConfig := &ssh.ClientConfig{
+		User: profile.Username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(profile.Password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	sftpConnection, err := ssh.Dial("tcp", profile.HostName+":"+strconv.Itoa(profile.Port), sshConfig)
+	if err != nil {
+		log.Fatal("Error connecting to SFTP:", err)
+	}
+	defer sftpConnection.Close()
+
+	sftpClient, err := sftp.NewClient(sftpConnection)
+	if err != nil {
+		log.Fatal("Error connecting to SFTP:", err)
+	}
+	defer sftpClient.Close()
+
+	// List files in the root directory
+	files, err := sftpClient.ReadDir("/")
+	if err != nil {
+		log.Fatalf("Failed to read directory: %s", err)
+	}
+
+	fmt.Println("Files in the root directory:")
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
 }
