8b75b3d4bc0ada3f274b1286b3d0f30a3ab9dea3
Hunter Barney <102537986+HunterBarney@users.noreply.github.com>
2024-12-09 16:42:34 -0500

    Added test for ftp downloads

--- ftp_test.go
+++ ftp_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"path/filepath"
+	"testing"
+)
 
 func TestFTPConnect(t *testing.T) {
 	mockClient := &Profile{
@@ -17,6 +20,68 @@ func TestFTPConnect(t *testing.T) {
 
 	if client == nil {
 		t.Fatal("Failed to create client")
+	}
+
+	e = DisconnectFTP(client)
+	if e != nil {
+		t.Fatal("Failed to disconnect: ", e)
+	}
+}
+
+func TestFTPDownloadFile(t *testing.T) {
+	mockClient := &Profile{
+		HostName:   "test.rebex.net",
+		Port:       21,
+		Username:   "demo",
+		Password:   "password",
+		OutputName: "backup_test",
+	}
+
+	client, e := ConnectFTP(mockClient)
+	if e != nil {
+		t.Fatal("Failed to connect: ", e)
+	}
+
+	if client == nil {
+		t.Fatal("Failed to create client")
+	}
+
+	remotePath := "pub/example/imap-console-client.png"
+	localPath := filepath.Join(mockClient.OutputName, filepath.Base(remotePath))
+	e = DownloadFileFTP(client, remotePath, localPath)
+	if e != nil {
+		t.Fatal("Failed to download: ", e)
+	}
+
+	e = DisconnectFTP(client)
+	if e != nil {
+		t.Fatal("Failed to disconnect: ", e)
+	}
+}
+
+func TestFTPDownloadDirectory(t *testing.T) {
+	mockClient := &Profile{
+		HostName:   "test.rebex.net",
+		Port:       21,
+		Username:   "demo",
+		Password:   "password",
+		OutputName: "backup_test_dir",
+	}
+
+	client, e := ConnectFTP(mockClient)
+	if e != nil {
+		t.Fatal("Failed to connect: ", e)
+	}
+
+	if client == nil {
+		t.Fatal("Failed to create client")
+	}
+
+	remotePath := "pub"
+	localPath := filepath.Join(mockClient.OutputName, filepath.Base(remotePath))
+	e = DownloadDirectoryFTP(client, remotePath, localPath)
+	if e != nil {
+		t.Fatal("Failed to download: ", e)
 	}
 
 	e = DisconnectFTP(client)
