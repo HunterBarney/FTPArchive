cc62ed58ed0fe74a82a484b85813e39c69e3a1bb
Hunter <huntbarney0914@gmail.com>
2025-02-06 09:53:42 -0500

    Updated build.sh

--- build.sh
+++ build.sh
@@ -10,12 +10,18 @@ echo "Building for MacOS (Arm)..."
 GOOS=darwin GOARCH=arm64 go build  -o $OUTPUT_DIR/ftparchive-mac-arm64 ./cmd/ftparchive
 
 # Build for Windows
-echo "Building for Windows..."
+echo "Building for Windows (x86)..."
 GOOS=windows GOARCH=amd64 go build  -o $OUTPUT_DIR/ftparchive-windows-amd64.exe ./cmd/ftparchive
 
+echo "Building for Windows (Arm)..."
+GOOS=windows GOARCH=arm64 go build  -o $OUTPUT_DIR/ftparchive-windows-arm.exe ./cmd/ftparchive
+
 # Build for Linux
-echo "Building for Linux..."
+echo "Building for Linux (x86)..."
 GOOS=linux GOARCH=amd64 go build  -o $OUTPUT_DIR/ftparchive-linux-amd64 ./cmd/ftparchive
+
+echo "Building for Linux (ARM)..."
+GOOS=linux GOARCH=arm64 go build  -o $OUTPUT_DIR/ftparchive-linux-arm ./cmd/ftparchive
 
 #Next, zip all the builds
 cd ./$OUTPUT_DIR
@@ -25,4 +31,6 @@ echo "Now zipping builds..."
 zip ftparchive-mac-amd64.zip ftparchive-mac-amd64
 zip ftparchive-mac-arm64.zip ftparchive-mac-arm64
 zip ftparchive-windows-amd64.zip ftparchive-windows-amd64.exe
+zip ftparchive-windows-arm.zip ftparchive-windows-arm.exe
 zip ftparchive-linux-amd64.zip ftparchive-linux-amd64
+zip ftparchive-linux-arm.zip ftparchive-linux-arm
